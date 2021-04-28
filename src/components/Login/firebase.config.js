0dec82d4db21b3beddfc26eda35d5752841126d5
Shakil Ahmed Chowdhury <76751268+shakil2chowdhury@users.noreply.github.com>
2021-04-28 20:42:40 +0600

    Update firebase.config.js

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCHVn8ZS_9WizTJSgN136CmYS_kG3HsMuA",
+    apiKey: "",
     authDomain: "mobo-fix.firebaseapp.com",
-    projectId: "mobo-fix",
-    storageBucket: "mobo-fix.appspot.com",
-    messagingSenderId: "554537692472",
-    appId: "1:554537692472:web:7b3bd80797ae5af65c25cd"
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   export default firebaseConfig;
